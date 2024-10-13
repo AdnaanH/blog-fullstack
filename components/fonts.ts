fdd73c9891e66916b179b20bbcd5b600263e67c3
Adnan Hamisi <ahamisi777@gmail.com>
2024-10-14 00:05:18 +0300

    Initial commit

---
+++ components/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Poppins, Nunito } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const nunito = Nunito({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+  
+export const poppins = Poppins({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
